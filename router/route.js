434621012d7c3a1b039f5a328bea0fc3586327b4
Amit Kumar <amit.kumar@settlin.in>
2016-01-18 14:47:28 +0530

    several file inserted

---
+++ router/route.js
@@ -0,0 +1,17 @@
+// Router.route('/', function () {
+//   layoutTemplate : "layoutTemplate"
+//   template: "home",
+// });
+
+// Router.route('/', function () {
+//   layoutTemplate : "layoutTemplate"
+//   template :"hello"
+//   // this.render('hello');
+// });
+//
+// Router.configure({
+//     layoutTemplate: 'layoutTemplate'
+// });
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
