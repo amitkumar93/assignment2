14f37ac912458e07b2f4f890bf0f77dcf3670136
Amit Kumar <amit.kumar@settlin.in>
2016-01-27 16:33:13 +0530

    changes done in all pges

--- router/route.js
+++ router/route.js
@@ -1,27 +1,4 @@
-// Router.route('/', function () {
-//   layoutTemplate : "layoutTemplate"
-//   template: "home",
-// });
-
-// Router.route('/', function () {
-//   layoutTemplate : "layoutTemplate"
-//   template :"hello"
-//   // this.render('hello');
-// });
-//
-// Router.configure({
-//     layoutTemplate: 'layoutTemplate'
-// });
-// Router.configure({
-//   layoutTemplate: 'ApplicationLayout'
-// });
-
-
-
-Router.route('home', {
-  path: '/',
-  layoutTemplate: 'complexLayout',
-  yieldTemplates: {
-    'myFooter': {to: 'footer'}
-  }
+Router.route("/", {
+	name: "homePage",
+	template: "home",
 });
